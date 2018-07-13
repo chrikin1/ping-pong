0e004bb7ef2531161690485fb8902ec85640f961
chrikin1 <chrisianbartoo@gmail.com>
2018-07-13 15:29:07 +0300

    css and index.html updates

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $("form#ping-pong").submit(function(event)  {
+  event.preventDefault();
+  var number = paraseInt($("input#number").val());
+  var result = numbers
