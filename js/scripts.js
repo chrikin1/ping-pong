e765cf57364021732eb2c0035112077966cfc767
chrikin1 <chrisianbartoo@gmail.com>
2018-07-13 15:59:48 +0300

    new changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event)  {
-  event.preventDefault();
-  var number = paraseInt($("input#number").val());
-  var result = numbers
+    event.preventDefault();
+    var number = paraseInt($("input#number").val());
+    var result = numbers(number);
+    $("#result").text(result);
+    $("#result").forEach(function(number) {
+    $("#list-items").append("<li>") + number + "</li>");
+    });
+    });
+  });
+var numbers = functions
