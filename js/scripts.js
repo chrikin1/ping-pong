e8d58064a34f8103b7f2b98f0590a9b824cef787
chrikin1 <chrisianbartoo@gmail.com>
2018-07-15 22:22:21 +0300

    sripts.js modification

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,5 @@
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event)  {
-    event.preventDefault();
-    var number = paraseInt($("input#number").val());
-    var result = numbers(number);
-    $("#result").text(result);
-    $("#result").forEach(function(number) {
-    $("#list-items").append("<li>") + number + "</li>");
-    });
-    });
-  });
-var numbers = functions
+  event.preventDefault();
+  var number = paraseInt($("input#number").val());
+  var result = numbers(number);
