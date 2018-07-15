66b6529f09e4fb2bd8a92d332d85eb5e8dea68e6
chrikin1 <chrisianbartoo@gmail.com>
2018-07-15 22:40:37 +0300

    modified commit

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,16 @@ function chris (result){
   for(index=1;index<=result;index++){
     if (index) % 15===0) {
       moringa.push("pingpong");
-    }
-    else if (index%5===0) {
+       }
+        else if (index%5===0) {
       moringa.push("pong");
-      }
-      else if (index% 3 === 0) {
+       }
+        else if (index% 3 === 0) {
         moringa.push("ping");
 
-        }
+       }
         else{
         moringa.push(index);
-        }
+       }
+    }
+  };
