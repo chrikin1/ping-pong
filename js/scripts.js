85e392b6965d2e6198311c598f9b5a4e7305915d
chrikin1 <chrisianbartoo@gmail.com>
2018-07-17 11:36:08 +0300

    new changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var moringas=[];
+var moringa=[];
 var index=1;
 function chris (y){
   for(index=1;index<=y;index++){
@@ -10,22 +10,25 @@ function chris (y){
        }
         else if (index% 3 === 0) {
         moringa.push("ping");
-
        }
         else{
         moringa.push(index);
        }
     }
-    index=index+1;
+    return moringa
   }
 
 
-  var y=parseInt($("#inputval"))
+// console.log(chris(12))
+
   $(document).ready(function() {
     $("#submit").click(function(event){
       event.preventDefault();
-      chris(index);
-      moringas.forEach(function(moringa){
+      $("#result").empty();
+      // console.log(chris(12))
+      var y=parseInt($("#inputval").val());
+      // console.log(chris(y))
+      chris(y).forEach(function(moringa){
         $("#list").append("<li>"+moringa+"</li>")
       });
     });
